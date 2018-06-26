b2dda73f758228988588ebdc9b9338fc9a83715a
jiro.kirameki <ec2-user@ip-172-31-47-48.us-east-2.compute.internal>
2018-06-26 03:28:56 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,8 +1,15 @@
- <?php
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
  
  use \App\Item;
 
   class ItemsController extends Controller
+  
   {
 
     public function create()
@@ -34,5 +41,16 @@
             'keyword' => $keyword,
             'items' => $items,
         ]);
+    }
+    
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
   }
